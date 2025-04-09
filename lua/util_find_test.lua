1a8c975c0fa94b2184f60a4b2948f4e028e4a875
Searidang Pa <dangsyncpa@gmail.com>
2025-04-08 21:26:35 -0400

    Save progress

--- lua/util_find_test.lua
+++ lua/util_find_test.lua
@@ -26,4 +26,45 @@ M.get_enclosing_test = function()
   return nil, nil
 end
 
+
+-- maybe this is not needed
+local function_query = [[
+(function_declaration
+  name: (identifier) @name
+  parameters: (parameter_list
+    (parameter_declaration
+      name: (identifier)
+      type: (pointer_type
+        (qualified_type
+          package: (package_identifier) @_package_name
+          name: (type_identifier) ))))
+)
+]]
+
+M.Find_all_tests = function (go_bufnr)
+  local query = vim.treesitter.query.parse('go', function_query)
+  local parser = vim.treesitter.get_parser(go_bufnr, 'go', {})
+  assert(parser, 'parser is nil')
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  assert(root, 'root is nil')
+
+  local res = {}
+  for _, node in query:iter_captures(root, go_bufnr, 0, -1) do
+    if node == nil then
+      return res
+    end
+    local nodeContent = vim.treesitter.get_node_text(node, go_bufnr)
+
+    -- all tests start with Test_
+    if not string.match(nodeContent, 'Test_') then
+      goto continue
+    end
+
+    res[nodeContent] = node:start() + 1
+    ::continue::
+  end
+  return res
+end
+
 return M
