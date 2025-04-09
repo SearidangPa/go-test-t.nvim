5ddbb2c6affa3372545d95a541e2089a1d0d6928
Searidang Pa <dangsyncpa@gmail.com>
2025-04-09 17:05:10 -0400

    Save progress

--- lua/terminal_test/terminal_test.lua
+++ lua/terminal_test/terminal_test.lua
@@ -4,14 +4,17 @@ local terminal_multiplexer = require 'terminal_test.terminal_multiplexer'
 
 ---@class terminalTest
 ---@field terminalTest.terminals TerminalMultiplexer
+---@field terminalTest.tests_info table<string, terminal.testInfo>
 ---@field terminalTest.test_in_terminal fun(test_info: terminal.testInfo)
 ---@field terminalTest.test_buf_in_terminals fun(test_command_format: string)
 ---@field terminalTest.test_nearest_in_terminal fun(test_command_format: string)
 ---@field terminalTest.test_tracked_in_terminal fun()
 ---@field terminalTest.view_enclosing_test fun()
 ---@field terminalTest.view_last_test_teriminal fun()
-local terminal_test = {}
-terminal_test.terminals = terminal_multiplexer.new()
+local terminal_test = {
+  terminals = terminal_multiplexer.new(),
+  tests_info = {}, ---@type table<string, terminal.testInfo>
+}
 
 ---@class terminal.testInfo
 ---@field name string
