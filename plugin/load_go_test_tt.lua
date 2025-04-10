08f1ba25941a468ee666ba5047bfe0e23b5a475a
Searidang Pa <dangsyncpa@gmail.com>
2025-04-10 10:51:36 -0400

    Rename `ReloadTesties` command to `ReloadTesTT` in `load_go_test_tt.lua`.

--- plugin/load_go_test_tt.lua
+++ plugin/load_go_test_tt.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_user_command('ReloadTesties', function()
+vim.api.nvim_create_user_command('ReloadTesTT', function()
   -- Easy Reloading
   package.loaded['go-test-tt'] = nil
   require('go-test-tt').setup()
