276abc78e5978cb4b611b93bd56a505e9bfd3959
Searidang Pa <dangsyncpa@gmail.com>
2025-04-13 17:24:40 -0400

    Refactor terminal test handling to improve error trace processing and update buffer status more efficiently.

--- lua/terminal_test/terminal_test.lua
+++ lua/terminal_test/terminal_test.lua
@@ -20,112 +20,14 @@ function terminal_test_M.new(opts)
   }
   self.ns_id = opts.ns_id or vim.api.nvim_create_namespace 'Terminal Test'
   self.test_command_format = opts.test_command_format or 'go test ./... -v -run %s\r'
-  self:setup_commands()
+  self:_setup_user_commands()
   return self
-end
-
-function terminal_test_M:validate_test_info(test_info)
-  assert(test_info.name, 'No test found')
-  assert(test_info.test_bufnr, 'No test buffer found')
-  assert(test_info.test_line, 'No test line found')
-  assert(test_info.test_command, 'No test command found')
-  assert(vim.api.nvim_buf_is_valid(test_info.test_bufnr), 'Invalid buffer')
-end
-
-function terminal_test_M:handle_test_passed(test_info, float_term_state, current_time, cb_update_tracker)
-  if not test_info.set_ext_mark then
-    vim.api.nvim_buf_set_extmark(test_info.test_bufnr, self.ns_id, test_info.test_line - 1, 0, {
-      virt_text = { { string.format('✅ %s', current_time) } },
-      virt_text_pos = 'eol',
-    })
-    test_info.set_ext_mark = true
-  end
-  test_info.status = 'pass'
-  float_term_state.status = 'pass'
-  self.tests_info[test_info.name] = test_info
-  vim.schedule(function() self.displayer:update_buffer(self.tests_info) end)
-  if cb_update_tracker then
-    cb_update_tracker(test_info)
-  end
-end
-
-function terminal_test_M:handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
-  if not test_info.set_ext_mark then
-    vim.api.nvim_buf_set_extmark(test_info.test_bufnr, self.ns_id, test_info.test_line - 1, 0, {
-      virt_text = { { string.format('❌ %s', current_time) } },
-      virt_text_pos = 'eol',
-    })
-    test_info.set_ext_mark = true
-  end
-  test_info.status = 'fail'
-  float_term_state.status = 'fail'
-  self.tests_info[test_info.name] = test_info
-  util_quickfix.add_fail_test(test_info)
-  vim.schedule(function() self.displayer:update_buffer(self.tests_info) end)
-  if cb_update_tracker then
-    cb_update_tracker(test_info)
-  end
-end
-
-function terminal_test_M:handle_error_trace(line, test_info, cb_update_tracker)
-  local file, line_num
-  if vim.fn.has 'win32' == 1 then
-    file, line_num = string.match(line, 'Error Trace:%s+([%w%p]+):(%d+)')
-  else
-    file, line_num = string.match(line, 'Error Trace:%s+([^:]+):(%d+)')
-  end
-
-  if file and line_num then
-    local error_bufnr = vim.fn.bufnr(file)
-    if error_bufnr then
-      vim.fn.sign_define('GoTestError', { text = '❌', texthl = 'DiagnosticError' })
-      vim.fn.sign_place(0, 'GoTestErrorGroup', 'GoTestError', error_bufnr, { lnum = line_num })
-    end
-    test_info.status = 'fail'
-    test_info.fail_at_line = line_num
-    self.tests_info[test_info.name] = test_info
-    vim.schedule(function() self.displayer:update_buffer(self.tests_info) end)
-    util_quickfix.add_fail_test(test_info)
-    if cb_update_tracker then
-      cb_update_tracker(test_info)
-    end
-  end
-end
-
-function terminal_test_M:process_one_line(line, test_info, float_term_state, current_time, cb_update_tracker)
-  self:handle_error_trace(line, test_info, cb_update_tracker)
-
-  if string.match(line, '--- FAIL') then
-    fidget.notify(string.format('%s fail', test_info.name), vim.log.levels.ERROR)
-    self:handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
-    return true
-  elseif string.match(line, 'FAIL') then
-    fidget.notify(string.format('%s fail', test_info.name), vim.log.levels.ERROR)
-    self:handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
-    return true
-  elseif string.match(line, '--- PASS') then
-    self:handle_test_passed(test_info, float_term_state, current_time, cb_update_tracker)
-    return true
-  end
-end
-
-function terminal_test_M:process_buffer_lines(buf, first_line, last_line, test_info, float_term_state, cb_update_tracker)
-  local lines = vim.api.nvim_buf_get_lines(buf, first_line, last_line, false)
-  local current_time = os.date '%H:%M:%S'
-
-  for _, line in ipairs(lines) do
-    local detach = self:process_one_line(line, test_info, float_term_state, current_time, cb_update_tracker)
-    if detach then
-      test_info.fidget_handle:finish()
-      return true
-    end
-  end
 end
 
 ---@param self terminalTest
 ---@param test_info terminal.testInfo
 function terminal_test_M:test_in_terminal(test_info, cb_update_tracker)
-  self:validate_test_info(test_info)
+  self:_validate_test_info(test_info)
   self.terminals:toggle_float_terminal(test_info.name)
   local float_term_state = self.terminals:toggle_float_terminal(test_info.name)
   vim.api.nvim_chan_send(float_term_state.chan, test_info.test_command .. '\n')
@@ -134,7 +36,7 @@ function terminal_test_M:test_in_terminal(test_info, cb_update_tracker)
   vim.schedule(function()
     vim.api.nvim_buf_attach(float_term_state.buf, false, {
       on_lines = function(_, buf, _, first_line, last_line)
-        return self_ref:process_buffer_lines(buf, first_line, last_line, test_info, float_term_state, cb_update_tracker)
+        return self_ref:_process_buffer_lines(buf, first_line, last_line, test_info, float_term_state, cb_update_tracker)
       end,
     })
   end)
@@ -226,13 +128,6 @@ function terminal_test_M:test_nearest_in_terminal()
   }
 end
 
-function terminal_test_M:test_tracked_in_terminal()
-  local terminal_tracker = require 'terminals.track_test_terminal'
-  for _, test_info in ipairs(terminal_tracker.track_test_list) do
-    self:test_in_terminal(test_info)
-  end
-end
-
 function terminal_test_M:view_enclosing_test_terminal()
   local util_find_test = require 'util_find_test'
   local test_name, _ = util_find_test.get_enclosing_test()
@@ -249,7 +144,111 @@ function terminal_test_M:view_last_test_terminal()
   self.terminals:toggle_float_terminal(test_name)
 end
 
-function terminal_test_M:setup_commands()
+--- === Private ===
+
+--- === Process Buffer Lines ===
+function terminal_test_M:_process_buffer_lines(buf, first_line, last_line, test_info, float_term_state, cb_update_tracker)
+  local lines = vim.api.nvim_buf_get_lines(buf, first_line, last_line, false)
+  local current_time = os.date '%H:%M:%S'
+
+  for _, line in ipairs(lines) do
+    local detach = self:_process_one_line(line, test_info, float_term_state, current_time, cb_update_tracker)
+    if detach then
+      test_info.fidget_handle:finish()
+      return true
+    end
+  end
+end
+
+function terminal_test_M:_handle_test_passed(test_info, float_term_state, current_time, cb_update_tracker)
+  if not test_info.set_ext_mark then
+    vim.api.nvim_buf_set_extmark(test_info.test_bufnr, self.ns_id, test_info.test_line - 1, 0, {
+      virt_text = { { string.format('✅ %s', current_time) } },
+      virt_text_pos = 'eol',
+    })
+    test_info.set_ext_mark = true
+  end
+  test_info.status = 'pass'
+  float_term_state.status = 'pass'
+  self.tests_info[test_info.name] = test_info
+  vim.schedule(function() self.displayer:update_buffer(self.tests_info) end)
+  if cb_update_tracker then
+    cb_update_tracker(test_info)
+  end
+end
+
+function terminal_test_M:_handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
+  if not test_info.set_ext_mark then
+    vim.api.nvim_buf_set_extmark(test_info.test_bufnr, self.ns_id, test_info.test_line - 1, 0, {
+      virt_text = { { string.format('❌ %s', current_time) } },
+      virt_text_pos = 'eol',
+    })
+    test_info.set_ext_mark = true
+  end
+  test_info.status = 'fail'
+  float_term_state.status = 'fail'
+  self.tests_info[test_info.name] = test_info
+  util_quickfix.add_fail_test(test_info)
+  vim.schedule(function() self.displayer:update_buffer(self.tests_info) end)
+  if cb_update_tracker then
+    cb_update_tracker(test_info)
+  end
+end
+
+function terminal_test_M:_handle_error_trace(line, test_info, cb_update_tracker)
+  local file, line_num
+  if vim.fn.has 'win32' == 1 then
+    file, line_num = string.match(line, 'Error Trace:%s+([%w%p]+):(%d+)')
+  else
+    file, line_num = string.match(line, 'Error Trace:%s+([^:]+):(%d+)')
+  end
+
+  if file and line_num then
+    local error_bufnr = vim.fn.bufnr(file)
+    if error_bufnr then
+      vim.fn.sign_define('GoTestError', { text = '❌', texthl = 'DiagnosticError' })
+      vim.fn.sign_place(0, 'GoTestErrorGroup', 'GoTestError', error_bufnr, { lnum = line_num })
+    end
+    test_info.status = 'fail'
+    test_info.fail_at_line = line_num
+    self.tests_info[test_info.name] = test_info
+    vim.schedule(function() self.displayer:update_buffer(self.tests_info) end)
+    util_quickfix.add_fail_test(test_info)
+    if cb_update_tracker then
+      cb_update_tracker(test_info)
+    end
+  end
+end
+
+function terminal_test_M:_process_one_line(line, test_info, float_term_state, current_time, cb_update_tracker)
+  self:_handle_error_trace(line, test_info, cb_update_tracker)
+
+  if string.match(line, '--- FAIL') then
+    fidget.notify(string.format('%s fail', test_info.name), vim.log.levels.ERROR)
+    self:_handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
+    return true
+  elseif string.match(line, 'FAIL') then
+    fidget.notify(string.format('%s fail', test_info.name), vim.log.levels.ERROR)
+    self:_handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
+    return true
+  elseif string.match(line, '--- PASS') then
+    self:_handle_test_passed(test_info, float_term_state, current_time, cb_update_tracker)
+    return true
+  end
+end
+
+--- === Validate Test Info ===
+
+function terminal_test_M:_validate_test_info(test_info)
+  assert(test_info.name, 'No test found')
+  assert(test_info.test_bufnr, 'No test buffer found')
+  assert(test_info.test_line, 'No test line found')
+  assert(test_info.test_command, 'No test command found')
+  assert(vim.api.nvim_buf_is_valid(test_info.test_bufnr), 'Invalid buffer')
+end
+
+--- === Setup user Commands ===
+function terminal_test_M:_setup_user_commands()
   local self_ref = self
   vim.api.nvim_create_user_command('TermTestSearch', function() self_ref.terminals:search_terminal() end, {})
   vim.api.nvim_create_user_command('TermTestLast', function() self_ref:view_last_test_terminal() end, {})
