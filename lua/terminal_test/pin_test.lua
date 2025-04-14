62e21d9c8bab81a77dbbac55e3c2605f507f0064
Searidang Pa <dangsyncpa@gmail.com>
2025-04-14 10:15:31 -0400

    Updated pin_tester to directly update terminal display without storing test info and added check for valid window before creating a new one.

--- lua/terminal_test/pin_test.lua
+++ lua/terminal_test/pin_test.lua
@@ -15,10 +15,11 @@ function pin_tester.new(opts)
 end
 
 function pin_tester:pin_test()
-  local test_info = self.term_tester:test_nearest_in_terminal()
-  table.insert(self.pinned_list, test_info)
+  self.term_tester:test_nearest_in_terminal()
   self.term_tester.displayer:update_buffer(self.pinned_list)
-  self.term_tester.displayer:toggle_display()
+  if not vim.api.nvim_win_is_valid(self.term_tester.displayer.display_win_id) then
+    self.term_tester.displayer:create_window_and_buf()
+  end
 end
 
 return pin_tester
