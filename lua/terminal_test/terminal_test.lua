3f90b96573e910438dddbbf57055b0bd7bd03442
Searidang Pa <dangsyncpa@gmail.com>
2025-04-14 09:49:30 -0400

    Changed notification level from ERROR to INFO for test pass/fail messages in terminal_test.lua.

--- lua/terminal_test/terminal_test.lua
+++ lua/terminal_test/terminal_test.lua
@@ -223,13 +223,15 @@ function terminal_test:_process_one_line(line, test_info, float_term_state, curr
 
   if string.match(line, '--- FAIL') then
     if test_info.fidget_handle then
-      fidget.notify(string.format('%s fail', test_info.name), vim.log.levels.ERROR)
+      local make_notify = require('mini.notify').make_notify {}
+      make_notify(string.format('%s fail', test_info.name), vim.log.levels.INFO)
     end
     self:_handle_test_failed(test_info, float_term_state, current_time, cb_update_tracker)
     return true
   elseif string.match(line, '--- PASS') then
     if test_info.fidget_handle then
-      fidget.notify(string.format('%s pass', test_info.name), vim.log.levels.ERROR)
+      local make_notify = require('mini.notify').make_notify {}
+      make_notify(string.format('%s pass', test_info.name), vim.log.levels.INFO)
     end
     self:_handle_test_passed(test_info, float_term_state, current_time, cb_update_tracker)
     return true
