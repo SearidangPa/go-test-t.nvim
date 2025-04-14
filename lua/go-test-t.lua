24d25adb11c492f2ea928661fddb308c9c953d41
SearidangPa <dangsyncpa@gmail.com>
2025-04-13 23:12:40 -0400

    Checkpoint: add test name in notify

--- lua/go-test-t.lua
+++ lua/go-test-t.lua
@@ -163,9 +163,9 @@ function go_test:_mark_outcome(entry)
   self.tests_info[key] = test_info
   if entry.Action == 'fail' then
     require('util_go_test_quickfix').add_fail_test(test_info)
-    require('fidget').notify('Test failed', vim.log.levels.ERROR)
+    require('fidget').notify(string.format('Test failed: %s', entry.Test), vim.log.levels.ERROR)
   elseif entry.Action == 'pass' then
-    require('fidget').notify('Test passed', vim.log.levels.INFO)
+    require('fidget').notify(string.format('Test passed: %s', entry.Test), vim.log.levels.INFO)
   end
 end
 
