7cf46eaf9f3fd62246f9eb345905fffb4831695b
Searidang Pa <dangsyncpa@gmail.com>
2026-03-15 07:12:43 +0700

    migrate-to-beaver

--- lua/go-test-t/util_find_test.lua
+++ lua/go-test-t/util_find_test.lua
@@ -6,64 +6,32 @@ function M.get_enclosing_test()
         return require("go-test-t.util_lua").get_enclosing_test()
     end
 
-    local node = vim.treesitter.get_node()
-    while node do
-        if node:type() ~= "function_declaration" then
-            node = node:parent() -- Traverse up the node tree to find a function node
-            goto continue
-        end
+    local beaver = require("beaver")
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local row0 = cursor[1] - 1
 
-        local func_name_node = node:child(1)
-        if func_name_node then
-            local func_name = vim.treesitter.get_node_text(func_name_node, 0)
-            local startLine, _, _ = node:start()
-            if not string.match(func_name, "Test") then
-                return nil
-            end
-            return func_name, startLine + 1 -- +1 to convert 0-based to 1-based lua indexing system
-        end
-        ::continue::
+    local ctx = beaver.enclosing_function(bufnr, row0)
+    if not ctx then
+        return nil, nil
     end
 
-    return nil, nil
+    if not string.match(ctx.name, "Test") then
+        return nil
+    end
+
+    return ctx.name, ctx.start_row + 1
 end
 
--- maybe this is not needed
-local function_query = [[
-(function_declaration
-  name: (identifier) @name
-  parameters: (parameter_list
-    (parameter_declaration
-      name: (identifier)
-      type: (pointer_type
-        (qualified_type
-          package: (package_identifier) @_package_name
-          name: (type_identifier) ))))
-)
-]]
-
 M.find_all_tests_in_buf = function(go_bufnr)
-    local query = vim.treesitter.query.parse("go", function_query)
-    local parser = vim.treesitter.get_parser(go_bufnr, "go", {})
-    assert(parser, "parser is nil")
-    local tree = parser:parse()[1]
-    local root = tree:root()
-    assert(root, "root is nil")
+    local beaver = require("beaver")
+    local funcs = beaver.list_functions(go_bufnr)
 
     local res = {}
-    for _, node in query:iter_captures(root, go_bufnr, 0, -1) do
-        if node == nil then
-            return res
+    for _, ctx in ipairs(funcs) do
+        if string.match(ctx.name, "Test") then
+            res[ctx.name] = ctx.start_row + 1
         end
-        local nodeContent = vim.treesitter.get_node_text(node, go_bufnr)
-
-        -- all tests start with Test
-        if not string.match(nodeContent, "Test") then
-            goto continue
-        end
-
-        res[nodeContent] = node:start() + 1
-        ::continue::
     end
     return res
 end
